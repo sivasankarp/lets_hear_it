dd0717be4522cdec6937a1bd7ab39bf6e2ea5043
Vikas Sharma <teckudos@gmail.com>
2020-08-09 10:16:18 +0530

    Update Constants.kt

--- app/src/main/java/com/teckudos/letshearit/utils/Constants.kt
+++ app/src/main/java/com/teckudos/letshearit/utils/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
     companion object {
         const val PERMISSIONS_REQUEST: Int = 101
         const val PITCH: Float = 6.0F
-        const val MAX: Int = 3
+        const val MAX: Int = 10
     }
 }
